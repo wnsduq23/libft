9abe23015c622e059d0b7b5142e073d7c8857498
Junyeop Kim <junykim@c4r2s3.42seoul.kr>
2022-11-01 23:12:27 +0900

    header + buffersize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: junykim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 11:50:27 by junykim           #+#    #+#             */
-/*   Updated: 2022/05/24 21:34:50 by junykim          ###   ########.fr       */
+/*   Updated: 2022/11/01 23:11:53 by junykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,8 @@ t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), \
 /*------------------------
 	  get_next_line.c
 ------------------------*/
+# define BUFFER_SIZE 2048
+
 typedef struct s_node
 {
 	int				fd;
